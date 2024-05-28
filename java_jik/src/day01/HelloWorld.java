3516bf8a9873fcf14023ec2cb2106e9fe3116406
st8324 <stajun@naver.com>
2024-05-28 17:10:36 +0900

    자바 1일차 : Hello world 예제

---
+++ java_jik/src/day01/HelloWorld.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class HelloWorld {
+
+	//한줄 주석입니다. // 다음부터 주석
+	/* 여러줄 주석 : 주석은 코드에 영향을 미치지 않습니다.
+	 */
+	/** 메서드 설명을 위한 주석
+	 */
+	//프로그램이 실행하기 위해서 main이 필요하다
+	public static void main(String[] args) {
+		//System.out.println : 콘솔에 원하는 문구를 출력하는 기능
+		System.out.println("Hello world!");
+	}
+
+}

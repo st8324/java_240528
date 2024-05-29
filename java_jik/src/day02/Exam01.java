224aaee4b77033e5101ba3c9801cbb44875dbe80
st8324 <stajun@naver.com>
2024-05-29 10:53:12 +0900

    자바 2일차 : 변수 - 복습 예제

---
+++ java_jik/src/day02/Exam01.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class Exam01 {
+	
+	public static void main(String[] args) {
+		/* 프로젝트에 day02 패키지를 생성하세요.
+		 * day02패키지에 Exam1 클래스를 생성하세요(main 체크)
+		 * Exam1 클래스에 문자형 변수 ch에 숫자0을 저장하고 콘솔에 출력하는 코드를 작성하세요.
+		 * */
+		char ch = '0';
+		System.out.println(ch);
+	}
+
+}

8ca74e61dd4a8cf5603ee21201c1e8e99b9723b4
st8324 <stajun@naver.com>
2024-05-29 11:00:22 +0900

    자바 2일차 : 변수 - 변수명 작성 규칙 예제

---
+++ java_jik/src/day02/VariableEx03.java
@@ -0,0 +1,22 @@
+package day02;
+
+public class VariableEx03 {
+	//변수명 작성 규칙 예제
+	public static void main(String[] args) {
+		int num;
+		int NUM, Num, nUm, nuM; //대소문자를 구별하기 때문에 가능
+
+		//int char, class, public;//예약어 사용 불가능
+		
+		//특수 문자는 _와 $만 가능
+		int _num, n_num, $num;
+		//int n um, #num;
+		
+		int num1;
+		//int 1num;//숫자는 앞에 올 수 없다
+		
+		//int num;//중복 선언 불가능. 현재 위치에 사용중인 변수가 1개인 경우 사용 가능
+				
+	}
+
+}

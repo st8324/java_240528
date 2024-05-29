6e19e409bb9fb5936e05e3147189d8fd35c260b6
st8324 <stajun@naver.com>
2024-05-29 10:53:52 +0900

    자바 2일차 : 변수 - 문자열 더하기 실습 예제

---
+++ java_jik/src/day02/Exam02.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		int num1 = 1, num2 = 2;
+		char ch = '+';
+		//num1, num2, ch를 이용하여 1+2가 콘솔에 출력되도록 코드를 작성해보세요.
+		String str = "" + num1 + ch + num2;
+		System.out.println("" + num1 + ch + num2);
+		System.out.println(str);
+	}
+
+}

9fa5e0143efbe0423c5a7b054283d305591b3a2b
st8324 <stajun@naver.com>
2024-05-29 11:00:31 +0900

    자바 2일차 : 변수 - 변수명 작성 관례 예제

---
+++ java_jik/src/day02/VariableEx04.java
@@ -0,0 +1,19 @@
+package day02;
+
+public class VariableEx04 {
+
+	//변수명 작성 관례
+	public static void main(String[] args) {
+		//Num대신 num를 추천
+		int Num, num;
+		//totalcount 대신 totalCount를 추천(카멜표기법) 
+		int totalcount, totalCount;
+		//상수는 대문자로 표현하는걸 추천
+		final int maxNum = 10, MAX_NUM = 10;
+		//MAX_NUM = 20;//위에서 값을 한 번 저장했기 때문에 상수는 값을 더 수정할 수 없다
+		//나이를 의미하는 변수로 age를 추천. 변수명은 의미전달이 잘 되도록 작성
+		int age, a;
+		
+	}
+
+}

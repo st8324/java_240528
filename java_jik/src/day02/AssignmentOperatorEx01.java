0d36f498b42005cf5d14dced7ebac77d55d22623
st8324 <stajun@naver.com>
2024-05-29 17:07:09 +0900

    자바 2일차 : 연산자 - 복합 대입 연산자

---
+++ java_jik/src/day02/AssignmentOperatorEx01.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class AssignmentOperatorEx01 {
+
+	public static void main(String[] args) {
+		//복합 대입 연산자 예제
+		int num1 = 10, num2 = 20;
+		
+		num1 = num1 + num2;
+		
+		System.out.println(num1);
+		
+		int num3 = 10, num4 = 20;
+		
+		num3 += num4;//복합대입연산자
+		
+		System.out.println(num3);
+
+	}
+
+}

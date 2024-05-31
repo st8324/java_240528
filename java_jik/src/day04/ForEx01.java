2785511cc865096ecd1ed75186924d06414b2063
st8324 <stajun@naver.com>
2024-05-31 17:15:01 +0900

    자바 4일차 : 반복문 - for문 - Hello world 출력

---
+++ java_jik/src/day04/ForEx01.java
@@ -0,0 +1,19 @@
+package day04;
+
+public class ForEx01 {
+
+	public static void main(String[] args) {
+		//Hello world 5번 출력 예제
+		/* 반복횟수 : i는 1부터 5까지 1씩 증가
+		 * 규칙성 : Hello world를 출력
+		 * 반복문 종료 후 : 없음
+		 * */
+		
+		int i;
+		for( i = 1 ; i<=5 ; i++ ) {
+			System.out.println("Hello world");
+		}
+
+	}
+
+}

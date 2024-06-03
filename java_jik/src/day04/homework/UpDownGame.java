e4170ea693c97c6df1e00e42ec6a2ce7aa4ddad4
st8324 <stajun@naver.com>
2024-06-03 13:43:10 +0900

    자바 5일차 : 반복문 - 과제 풀이

--- java_jik/src/day04/homework/UpDownGame.java
+++ java_jik/src/day04/homework/UpDownGame.java
@@ -1,5 +1,7 @@
 package day04.homework;
 
+import java.util.Scanner;
+
 public class UpDownGame {
 
 	public static void main(String[] args) {
@@ -16,7 +18,34 @@ public class UpDownGame {
 		 * */
 		int min = 1, max = 100;
 		int random = (int)(Math.random() * (max - min + 1) + min);
+		
+		Scanner scan = new Scanner(System.in);
+		int num;
 		System.out.println("랜덤한 수 : " + random);
+		do {
+			//정수 입력 : 이라는 안내 문구 출력 
+			System.out.print("정수 입력 : ");
+			//정수를 입력 
+			num = scan.nextInt();
+			//DOWN인지 UP인지 정답인지를 판별
+			/* num가 random보다 크다면 DOWN!으로 출력하고
+			 * 아니면 num가 random보다 작으면 UP!으로 출력하고
+			 * 아니면 정답입니다라고 출력
+			 * */
+			if(num > random) {
+				System.out.println("DOWN!");
+			}
+			else if(num < random) {
+				System.out.println("UP!");
+			}
+			else {
+				System.out.println("정답입니다!");
+			}
+		}while(random != num);
 	}
-
 }
+
+
+
+
+

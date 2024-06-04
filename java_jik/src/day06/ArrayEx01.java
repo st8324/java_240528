179162056c2e3dbe5c66aceb9454a0713a3b008b
st8324 <stajun@naver.com>
2024-06-04 14:19:48 +0900

    자바 6일차 : 배열 - 배열 선언 방법에 따른 차이

---
+++ java_jik/src/day06/ArrayEx01.java
@@ -0,0 +1,18 @@
+package day06;
+
+public class ArrayEx01 {
+
+	public static void main(String[] args) {
+		//배열 선언 방법에 따른 차이점을 보여주는 예제
+		int [] arr1, arr2;
+		int arr3[], arr4;
+		
+		arr1 = new int[3]; //arr1은 배열
+		arr2 = new int[3]; //arr2은 배열
+		arr3 = new int[3]; //arr3은 배열
+		//arr4 = new int[3]; //arr4는 배열이 아님
+		arr4 = 10; //arr4는 변수
+
+	}
+
+}

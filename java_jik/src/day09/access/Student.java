687e17b35f31d1a4398f06d33246f48de1dcbd19
st8324 <stajun@naver.com>
2024-06-10 13:56:34 +0900

    자바 9일차 : 클래스 - 접근제어자 예제

---
+++ java_jik/src/day09/access/Student.java
@@ -0,0 +1,8 @@
+package day09.access;
+
+public class Student {
+	public int grade;
+	protected int classNum;
+	int num;//default
+	private String name;
+}
---
+++ java_jik/src/day09/access2/AccessModifierEx02.java
@@ -0,0 +1,16 @@
+package day09.access2;
+
+import day09.access.Student;
+
+public class AccessModifierEx02 {
+
+	/* 접근 제어자 예제*/
+	public static void main(String[] args) {
+		Student std1 = new Student();
+		std1.grade = 1;//접근제어자가 public이어서 다른 클래스에서 사용 가능
+		//std1.classNum = 1; //접근제어가 protected이어서 다른 패키지에 있는 다른 클래스에서 사용 불가능 
+		//std1.num = 1;//접근제어자가 default이어서 다른 패키지에 있는 다른 클래스에서 사용 불가능
+		//std1.name = "홍길동";//접근제어자가 private이어서 다른 클래스에서 사용이 불가능 
+	}
+
+}

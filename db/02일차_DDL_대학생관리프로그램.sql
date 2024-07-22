f64780b757755a20f90e6452390f4fd012b8072c
st8324 <stajun@naver.com>
2024-07-22 15:53:15 +0900

    DB 02일차 : DDL - 대학생 관리 프로그램

--- db/02일차_DDL_대학생관리프로그램.sql
+++ db/02일차_DDL_대학생관리프로그램.sql
@@ -14,4 +14,46 @@ CREATE TABLE IF NOT EXISTS student(
     st_name varchar(30) not null,
     st_major varchar(15) not null,
     st_grade int not null default 1
-)
+);
+
+# professor 테이블 삭제
+DROP TABLE IF EXISTS professor;
+# professor 테이블 추가
+# 학번, 이름(30자), 전공(15자), 교수길(100)
+CREATE TABLE IF NOT EXISTS professor(
+	pr_num char(10) primary key,
+    pr_name varchar(30) not null,
+    pr_major varchar(15) not null,
+    pr_room varchar(100)
+);
+
+# lecture 테이블 삭제
+DROP TABLE IF EXISTS lecture;
+# lecture 테이블 추가
+# 강의 번호(PK), 강의명(30자), 강의실(100자), 강의 시간(시간표, 최대 50자), 
+# 학점, 시수, 전공 여부(전공 선택, 전공 필수, 교양 선택, 교양 필수), 교수 번호(FK)
+CREATE TABLE IF NOT EXISTS lecture(
+	le_num int primary key auto_increment, 
+    le_title varchar(30) not null,
+    le_major char(5) not null,
+    le_room varchar(100),
+    le_schedule varchar(50), 
+    le_point int,
+    le_time int,
+    le_pr_num char(10),
+    foreign key(le_pr_num) references professor(pr_num)
+);
+# 수강 테이블 삭제
+DROP TABLE IF EXISTS course;
+# course 테이블 추가
+# 수강 번호(PK),중간, 기말, 과제, 학점, 출석, 학번(FK), 강의 번호(FK)
+CREATE TABLE IF NOT EXISTS course(
+
+);
+# 연락처 테이블 삭제
+DROP TABLE IF EXISTS contact;
+# contact 테이블 추가
+# 연락처 번호(PK), 주소(100자), 상세 주소(100자), 전화 번호(최대 13자)
+CREATE TABLE IF NOT EXISTS contact(
+
+);

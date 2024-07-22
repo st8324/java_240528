e90ed991987baf53e436ac971efa05cc2304b3e5
st8324 <stajun@naver.com>
2024-07-22 17:09:33 +0900

    DB 02일차 : 파일명 변경

--- db/02일차_대학생관리프로그램_DDL.sql
+++ db/02일차_대학생관리프로그램_DDL.sql
@@ -48,12 +48,27 @@ DROP TABLE IF EXISTS course;
 # course 테이블 추가
 # 수강 번호(PK),중간, 기말, 과제, 학점, 출석, 학번(FK), 강의 번호(FK)
 CREATE TABLE IF NOT EXISTS course(
-
+	co_num int primary key auto_increment,
+    co_mid int default 0,
+    co_final int default 0,
+    co_perform int default 0,
+    co_homework int default 0,
+    co_total varchar(2) default 'F',
+    co_attendance int default 0,
+    co_st_num char(10),
+    co_le_num int,
+    foreign key(co_st_num) references student(st_num),
+    foreign key(co_le_num) references lecture(le_num)
 );
 # 연락처 테이블 삭제
 DROP TABLE IF EXISTS contact;
 # contact 테이블 추가
-# 연락처 번호(PK), 주소(100자), 상세 주소(100자), 전화 번호(최대 13자)
+# 연락처 번호(PK), 주소(100자), 상세 주소(100자), 전화 번호(최대 13자), 학번(FK)
 CREATE TABLE IF NOT EXISTS contact(
-
+	ct_num int primary key auto_increment,
+    ct_addr varchar(100) not null,
+    ct_detail varchar(100),
+    ct_phone varchar(13) not null,
+	ct_st_num char(10) not null,
+    foreign key(ct_st_num) references student(st_num)
 );

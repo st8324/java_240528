112a81b655906447a477f5c51a557d05211f1a64
st8324 <stajun@naver.com>
2024-07-25 12:39:43 +0900

    DB 05일차 : 프로시저

---
+++ db/05일차_트리거.sql
@@ -0,0 +1,43 @@
+
+USE CGV;
+
+# 상영시간에 예약 가능한 초기 좌석 수를 상영관 좌석수로 수정 
+UPDATE SCHEDULE
+        JOIN
+    SCREEN ON SC_NUM = SD_SC_NUM 
+SET 
+    SD_POSSIBLE = SC_SEAT;
+
+# 예약된 좌석만큼 상영시간에서 예약 가능한 좌석수를 빼서 수정하는 쿼리
+
+# 1번 스케쥴에 예매된 좌석 수만큼 1번 스케쥴의 좌석을 수정하는 쿼리 
+UPDATE 
+	SCHEDULE
+SET
+	SD_POSSIBLE = SD_POSSIBLE - (SELECT SUM(TI_ADULT + TI_TEENAGER) FROM TICKETING WHERE TI_SD_NUM = 1)
+WHERE
+	SD_NUM = 1;
+
+# 예매가 발생하면 예매한 상영에서 예약 가능한 좌석수를 수정하는 트리거 
+DROP TRIGGER IF EXISTS TICKETING_INSERT;
+DELIMITER //
+CREATE TRIGGER TICKETING_INSERT
+AFTER INSERT ON TICKETING 
+FOR EACH ROW
+BEGIN
+
+	# 예매된 성인수와 청소년수만큼 좌석수를 수정 
+    UPDATE 
+		SCHEDULE
+	SET
+		SD_POSSIBLE = SD_POSSIBLE - (NEW.TI_ADULT + NEW.TI_TEENAGER)
+	WHERE
+		SD_NUM = NEW.TI_SD_NUM;
+END //
+DELIMITER ;
+
+INSERT INTO TICKETING VALUES(NULL, 2, 2, 48000, 'abc123', 2);
+INSERT INTO TICKETING_LIST VALUES(NULL, 2, 1),(NULL, 2, 2),(NULL, 2, 3),(NULL, 2, 4);
+
+# 등록된 트리거 조회
+SHOW TRIGGERS;

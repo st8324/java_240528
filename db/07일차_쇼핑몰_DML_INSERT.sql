4d61cded42c06110f9480152dd9425f67f377e10
st8324 <stajun@naver.com>
2024-07-30 17:03:54 +0900

    DB 08일차 : 쇼핑몰 - DML

--- db/07일차_쇼핑몰_DML_INSERT.sql
+++ db/07일차_쇼핑몰_DML_INSERT.sql
@@ -39,3 +39,36 @@ INSERT INTO PRODUCT(PR_CODE, PR_NAME, PR_CONTENT, PR_PRICE, PR_CA_NUM)
 SELECT 'ACC001', '금 목걸이', '부의 상징.', '100000', CA_NUM 
 FROM CATEGORY 
 WHERE CA_NAME = '악세서리';
+
+# abc123회원이 CLO001 제품을 장바구니에 3개 담았을 때 쿼리 
+INSERT INTO BASKET(BA_PR_CODE, BA_ME_ID, BA_AMOUNT) VALUES('CLO001', 'abc123', 3);
+# abc123회원이 CLO001 제품을 장바구니에 2개 담았을 때 쿼리 
+UPDATE BASKET SET BA_AMOUNT = 2 WHERE BA_PR_CODE = 'CLO001' AND BA_ME_ID = 'abc123';
+
+# abc123회원이 ACC001 제품을 장바구니에 1개 담았을 쿼리
+INSERT INTO BASKET(BA_PR_CODE, BA_ME_ID, BA_AMOUNT) VALUES('ACC001', 'abc123', 1);
+# abc123회원이 장바구니에 있는 모든 제품을 구매했을 때 필요한 모든 쿼리 
+INSERT INTO BUY(BU_PR_CODE, BU_ME_ID, BU_AMOUNT, BU_STATE, BU_DATE)
+VALUES('CLO001', 'abc123', 3, '구매', NOW()),
+('ACC001', 'abc123', 1, '구매', NOW());
+
+# 장바구니에서 구매를하면 장바구니에 있는 항목을 제거해야 함. 
+DELETE FROM BASKET WHERE BA_PR_CODE = 'CLO001' AND BA_ME_ID = 'abc123';
+DELETE FROM BASKET WHERE BA_PR_CODE = 'ACC001' AND BA_ME_ID = 'abc123';
+
+# abc123회원이 id : abc123, pw : abc1234로 로그인 시도했을 때 실행해야하는 쿼리
+# => 로그인 실패 => 로그인 실패회수 증가
+UPDATE MEMBER SET ME_FAIL = ME_FAIL + 1 WHERE ME_ID = 'abc123';
+
+# abc123회원이 로그인 시도가 성공했을 때 실행해야하는 쿼리 
+UPDATE MEMBER SET ME_FAIL = 0 WHERE ME_ID = 'abc123';
+
+# abc123회원이 비번 찾기를 시도해서 인증코드가 123asd가 발급됐을 때 실행해야하는 쿼리 
+INSERT INTO CODE(CO_ME_ID, CO_CODE, CO_LIMIT) 
+VALUES('abc123', '123asd', DATE_ADD(NOW(), INTERVAL 5 MINUTE));
+
+# abc123회원이 인증코드를 입력해서 비번을 abc1234로 수정했을 때 실행해야하는 쿼리 
+UPDATE MEMBER SET ME_PW = 'abc1234' WHERE ME_ID = 'abc123';
+# 인증코드를 통해 비번을 변경했으면 인증코드를 삭제 
+DELETE FROM CODE WHERE CO_ME_ID = 'abc123';
+
--- db/07일차_쇼핑몰_DML_SELECT.sql
+++ db/07일차_쇼핑몰_DML_SELECT.sql
@@ -6,3 +6,70 @@ FROM
 RIGHT JOIN
 	CATEGORY ON CA_NUM = PR_CA_NUM
 GROUP BY CA_NUM;
+
+# 분류중에서 옷을 클릭했을 때 => 옷으로 등록된 제품들을 조회하는 쿼리 
+SELECT 
+    *
+FROM
+    PRODUCT
+        JOIN
+    CATEGORY ON PR_CA_NUM = CA_NUM
+WHERE
+    CA_NAME = '옷';
+
+# '시원한'이 들어간 제품을 검색 => 제목에 '시원한'이 포함된 제품을 조회하는 쿼리 
+SELECT 
+    *
+FROM
+    PRODUCT
+WHERE
+    PR_NAME LIKE CONCAT('%', '시원한', '%');
+# 누적 판매량이 가장 많은 제품들을 조회하는 쿼리
+SELECT 
+    DENSE_RANK() OVER(ORDER BY SUM(BU_AMOUNT) DESC) 판매순위,PRODUCT.*, SUM(BU_AMOUNT) 판매량
+FROM
+    PRODUCT
+        LEFT JOIN
+    (SELECT * FROM BUY WHERE BU_STATE IN('구매','구매확정')) B ON BU_PR_CODE = PR_CODE
+GROUP BY PR_CODE;
+
+# 옷 제품들 중에서 가격이 높은 순으로 제품을 조회하는 쿼리 
+SELECT 
+    PRODUCT.*
+FROM
+    PRODUCT
+        JOIN
+    CATEGORY ON PR_CA_NUM = CA_NUM
+WHERE
+    CA_NAME = '옷'
+ORDER BY PR_PRICE DESC;
+
+# abc123회원이 구매 목록을 조회하는 쿼리 
+SELECT 
+    DISTINCT BU_PR_CODE
+FROM
+    BUY
+WHERE
+    BU_ME_ID = 'abc123'
+        AND BU_STATE IN ('구매' , '구매확정');
+
+# abc123회원이 누적 구매 금액을 조회하는 쿼리 
+SELECT 
+    BU_ME_ID, FORMAT(SUM(PR_PRICE * BU_AMOUNT),0) 누적구매금액
+FROM
+    BUY
+        JOIN
+    PRODUCT ON PR_CODE = BU_PR_CODE
+WHERE BU_STATE IN ('구매', '구매확정') AND BU_ME_ID = 'abc123'
+GROUP BY BU_ME_ID;
+
+# 회원별 누적 금액을 조회하는 쿼리 
+SELECT 
+    ME_ID, IFNULL(FORMAT(SUM(PR_PRICE * BU_AMOUNT),0), 0) 누적구매금액
+FROM
+	MEMBER
+		LEFT JOIN
+    (SELECT * FROM BUY WHERE BU_STATE IN ('구매', '구매확정')) B ON ME_ID = BU_ME_ID
+        LEFT JOIN
+    PRODUCT ON PR_CODE = BU_PR_CODE
+GROUP BY ME_ID;

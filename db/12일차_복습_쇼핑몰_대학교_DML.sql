97281bc3ddf6884d5fa81b51bb986f9088b7f573
st8324 <stajun@naver.com>
2024-08-05 17:06:33 +0900

    DB 12일차 : 복습 DML - 쇼핑몰, 대학교

---
+++ db/12일차_복습_쇼핑몰_대학교_DML.sql
@@ -0,0 +1,71 @@
+# 모든 제품을 조회하는 쿼리 
+SELECT * FROM PRODUCT;
+# 금액이 1만원 이상인 제품을 조회하는 쿼리 
+SELECT * FROM PRODUCT WHERE PR_PRICE >= 10000;
+# 제품명에 시원한이 들어간 제품을 조회하는 쿼리 
+SELECT * FROM PRODUCT WHERE PR_NAME LIKE CONCAT('%', '시원한', '%');
+# 카테고리가 옷인 제품을 조회하는 쿼리 
+SELECT * FROM PRODUCT WHERE PR_CA_NUM = 1;
+
+# 금액과 제품명을 이용해서 검색하는 쿼리를 일반화해서 만들어보세요. 
+SELECT * FROM PRODUCT WHERE PR_NAME LIKE CONCAT('%', '', '%') AND PR_PRICE >= 10000;
+
+SELECT * FROM PRODUCT 
+WHERE PR_NAME LIKE CONCAT('%', '', '%') 
+	AND PR_PRICE >= 10000 
+    AND PR_CA_NUM = 1;
+
+# 카테고리별 제품의 평균가격을 조회하는 쿼리 
+SELECT CA_NAME, FORMAT(IFNULL(AVG(PR_PRICE), 0),0) 평균가격 FROM CATEGORY 
+LEFT JOIN PRODUCT ON PR_CA_NUM = CA_NUM
+GROUP BY CA_NUM;
+
+# 카테도리별 제품의 평균 가격이 3만원이상인 카테고리를 조회 
+SELECT CA_NAME, FORMAT(IFNULL(AVG(PR_PRICE), 0),0) 평균가격 FROM CATEGORY 
+LEFT JOIN PRODUCT ON PR_CA_NUM = CA_NUM
+GROUP BY CA_NUM
+HAVING IFNULL(AVG(PR_PRICE), 0) >= 30000;
+
+# 카테고리별 등록된 제품수를 조회 
+SELECT CA_NAME, COUNT(PR_CA_NUM) 제품수 FROM CATEGORY 
+LEFT JOIN PRODUCT ON PR_CA_NUM = CA_NUM
+GROUP BY CA_NUM;
+
+# 'abc123'회원이 구매한 제품 정보를 조회 
+SELECT PRODUCT.* FROM BUY 
+JOIN PRODUCT ON PR_CODE = BU_PR_CODE
+WHERE BU_ME_ID = 'abc123' AND BU_STATE IN('구매', '구매 확정');
+# 'abc123'회원의 누적 구매 금액을 조회 
+SELECT FORMAT(SUM(PR_PRICE * BU_AMOUNT),0) 누적금액 FROM BUY 
+JOIN PRODUCT ON PR_CODE = BU_PR_CODE
+WHERE BU_ME_ID = 'abc123' AND BU_STATE IN('구매', '구매 확정');
+
+# abc123 회원이 장바구니에 1번(CLO001) 제품 3개를 담았을 때 쿼리 
+INSERT INTO BASKET(BA_PR_CODE, BA_ME_ID, BA_AMOUNT)
+VALUES('CLO001', 'abc123',	3);
+# abc123회원이 장바구니에 1번 제품 2개를 담았을 때 쿼리 
+UPDATE BASKET SET BA_AMOUNT = 2 WHERE BA_PR_CODE = 'CLO001' AND BA_ME_ID = 'abc123';
+
+# abc123회원이 장바구니에 있는 `CLO001`제품을 2개 구매했을 때 쿼리 
+INSERT INTO BUY(BU_PR_CODE, BU_ME_ID, BU_AMOUNT, BU_STATE, BU_DATE)
+VALUES('CLO001', 'abc123', 2, '구매', now());
+DELETE FROM BASKET WHERE BA_ME_ID = 'abc123' AND BA_PR_CODE = 'CLO001';
+
+# 등록된 강의를 조회하는 쿼리 
+SELECT * FROM LECTURE;
+# 교수 번호가 2024123001인 교수님이 강의하는 강의를 조회하는 쿼리 
+SELECT * FROM LECTURE WHERE LE_PR_NUM = '2024123001';
+# 학번이 2024123001인 학생이 수강하는 강의를 조회하는 쿼리 
+SELECT * FROM LECTURE
+JOIN COURSE ON CO_LE_NUM = LE_NUM
+WHERE CO_ST_NUM = '2024123001';
+
+# 전공 필수인 강의를 조회하는 쿼리 
+SELECT * FROM LECTURE WHERE LE_MAJOR = '전공 필수';
+
+# 컴퓨터공학 학생들이 수강하는 강의들을 조회하는 쿼리 
+SELECT DISTINCT LECTURE.* FROM LECTURE 
+JOIN COURSE ON CO_LE_NUM = LE_NUM
+WHERE CO_ST_NUM IN (SELECT ST_NUM FROM STUDENT WHERE ST_MAJOR = '컴퓨터공학');
+
+

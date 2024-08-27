90203f90458d790b50e2415d6f6abe2222a9c79c
st8324 <stajun@naver.com>
2024-08-27 15:37:57 +0900

    스프링 02일차 : 회원 가입 기능 구현

--- spring projects/spring2/src/main/java/kr/kh/spring2/dao/MemberDAO.java
+++ spring projects/spring2/src/main/java/kr/kh/spring2/dao/MemberDAO.java
@@ -1,5 +1,11 @@
 package kr.kh.spring2.dao;
 
+import org.apache.ibatis.annotations.Param;
+
+import kr.kh.spring2.model.vo.MemberVO;
+
 public interface MemberDAO {
+
+	boolean insertMember(@Param("m")MemberVO member);
 
 }
--- spring projects/spring2/src/main/java/kr/kh/spring2/service/MemberService.java
+++ spring projects/spring2/src/main/java/kr/kh/spring2/service/MemberService.java
@@ -1,6 +1,9 @@
 package kr.kh.spring2.service;
 
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.kh.spring2.dao.MemberDAO;
@@ -11,9 +14,33 @@ public class MemberService {
 
 	@Autowired
 	private MemberDAO memberDao;
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
 
 	public boolean signup(MemberVO member) {
-		// TODO Auto-generated method stub
+		if(member==null) {
+			return false;
+		}
+		if(!checkRegex(member.getMe_id(), "^\\w{6,13}$")) {
+			return false;
+		}
+		if(!checkRegex(member.getMe_pw(), "^[a-zA-Z0-9!@#$]{6,15}$")) {
+			return false;
+		}
+		try {
+			String encPw = passwordEncoder.encode(member.getMe_pw());
+			member.setMe_pw(encPw);
+			return memberDao.insertMember(member);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	private boolean checkRegex(String str, String regex) {
+		if(str != null && Pattern.matches(regex, str)) {
+			return true;
+		}
 		return false;
 	}
 }

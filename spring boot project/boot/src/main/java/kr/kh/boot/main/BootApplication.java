6cbc37ebbdbf865281fd0a1a544ea9a3e3fd337e
st8324 <stajun@naver.com>
2024-09-24 15:23:35 +0900

    부트 01일차 : 부트 프로젝트 생성 및 기본 설정 적용

---
+++ spring boot project/boot/src/main/java/kr/kh/boot/main/BootApplication.java
@@ -0,0 +1,13 @@
+package kr.kh.boot.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+
+}
---
+++ spring boot project/boot/src/main/java/kr/kh/boot/main/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kr.kh.boot.main;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootApplication.class);
+	}
+
+}

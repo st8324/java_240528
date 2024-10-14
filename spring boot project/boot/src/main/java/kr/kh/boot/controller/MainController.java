3c3414258d63d2a0747914b4fd86c0aaa2fada31
st8324 <stajun@naver.com>
2024-10-14 11:44:34 +0900

    부트 : 로그인한 회원 정보 가져오기

--- spring boot project/boot/src/main/java/kr/kh/boot/controller/MainController.java
+++ spring boot project/boot/src/main/java/kr/kh/boot/controller/MainController.java
@@ -1,10 +1,11 @@
 package kr.kh.boot.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.kh.boot.model.util.CustomUser;
 
 @Controller
 public class MainController {
@@ -12,9 +13,9 @@ public class MainController {
 
 	@GetMapping("/")
 	public String home(Model model,
-			@AuthenticationPrincipal UserDetails userDatails) {
+			@AuthenticationPrincipal CustomUser userDatails) {
 		if(userDatails != null)
-			System.out.println(userDatails.getUsername());
+			System.out.println(userDatails.getMember().getMe_pw());
 		model.addAttribute("name", "홍길동");
 		return "home";
 	}
